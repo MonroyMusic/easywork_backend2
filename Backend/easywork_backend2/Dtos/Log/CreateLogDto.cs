161d5102b5a49a832b1e5e383614b9a31725f2e8
Gonzalo Monroy <123608502+MonroyMusic@users.noreply.github.com>
2024-05-03 23:06:45 -0600

    Add files via upload

---
+++ Backend/easywork_backend2/Dtos/Log/CreateLogDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace easywork_backend2.Dtos.Log
+{
+    public class CreateLogDto
+    {
+
+        public Guid Id { get; set; }
+
+        public string Action { get; set; }
+
+        public DateTime Time { get; set; }
+
+    }
+}
---
+++ Backend/easywork_backend2/Dtos/Log/LogDto.cs
@@ -0,0 +1,13 @@
+﻿namespace easywork_backend2.Dtos.Log
+{
+    public class LogDto
+    {
+
+        public string Action { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string User_Id { get; set; }
+
+    }
+}
