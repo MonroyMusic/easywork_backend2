3f670cd80c42a9d8e385077b8ca9d77e30f3ce6e
Gonzalo Monroy <123608502+MonroyMusic@users.noreply.github.com>
2024-05-03 23:06:16 -0600

    Add files via upload

---
+++ Backend/easywork_backend2/Entitys/Log/LogEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace easywork_backend2.Entitys.Log
+{
+    public class LogEntity
+    {
+
+        [Key][Required] public Guid Id { get; set; }
+
+        [Required] public string Action { get; set; }
+
+        [Required] public DateTime Time { get; set; }
+
+        [Required] public string User_Id { get; set; }
+
+    }
+}
